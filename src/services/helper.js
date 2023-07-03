6a979a1f03003d64d724f15314a3d400d9a5b8ae
suhailkhazirbhat <98658034+suhailkhazirbhat@users.noreply.github.com>
2023-07-03 11:43:12 +0530

    Update helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://https://modern-curd-backend-fa8bq36sy-suhailkhazirbhat.vercel.app/:6010"
+// export const BASE_URL = "http://https://modern-curd-backend-fa8bq36sy-suhailkhazirbhat.vercel.app/"
+export const BASE_URL = "http://localhost:6010"
 // export const BASE_URL = "https://mernappbackend.onrender.com"
